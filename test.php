c7303b366feef84ba72bf6dd76d479616f8abd71
AbraaoAbe <79542473+AbraaoAbe@users.noreply.github.com>
2022-05-04 01:25:35 -0300

    pagina finalizada
    Utilizando práticas do html e do css para criar uma pagina (zoeira xD)

---
+++ test.php
@@ -0,0 +1,25 @@
+<html>
+
+	<head>
+	
+		<title>Karininha safadinha </title>
+	
+	</head>
+
+	<body>
+		Anus anais
+	</body>
+	
+	<body>
+	
+		<?php
+		
+			for ( $i = 0; $i < 10; $i++){
+				print( "Linha numero " . $i . "<br />");
+			}
+		?>
+		
+	</body>
+	
+
+/<html>
